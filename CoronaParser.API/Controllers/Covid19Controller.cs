77ce3e5bd860b2822a94c2a22f6154e52761ade3
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-22 01:22:40 +0300

    fetch and parse covid19 api

---
+++ CoronaParser.API/Controllers/Covid19Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+// newtonsoft
+using Newtonsoft.Json;
+
+namespace CoronaParser.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Covid19Controller : ControllerBase
+    {
+        public Covid19Controller()
+        {
+
+        }
+
+        [HttpGet("covidresults")]
+        public async Task<ActionResult<CovidResult>> Get()
+        {
+            CovidResult result;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = string.Format("https://covid19.mathdro.id/api");
+                    var response = client.GetAsync(url).Result;
+                    
+                    string responseAsString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<CovidResult>(responseAsString);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+
+            return Ok(result);
+        }
+    }
+
+    public class CovidResult
+    {
+        [JsonProperty("confirmed")]
+        public ValueModel Confirmed { get; set; }
+        [JsonProperty("recovered")]
+        public ValueModel Recovered { get; set; }
+        [JsonProperty("deaths")]
+        public ValueModel Deaths { get; set; }
+    }
+
+    public class ValueModel
+    {
+        [JsonProperty("value")]
+        public int Value { get; set; }
+    }
+}
